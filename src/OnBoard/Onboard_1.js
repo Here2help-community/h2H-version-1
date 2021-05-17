e2d46eb95ed626dc8fa57f3240f409ce5af7900d
silenteyesoncode <46851135+silenteyesoncode@users.noreply.github.com>
2021-05-17 22:54:22 +0530

    Screen_css

--- src/OnBoard/Onboard_1.js
+++ src/OnBoard/Onboard_1.js
@@ -1,17 +1,27 @@
 import React from "react";
-import { View, Image, Text } from "react-native";
+import { View, Image, Text, StyleSheet } from "react-native";
 import AppText from '../component/AppText/AppText';
 import styles from './OnboardStyles';
 
 const Onboard_screen = () => {
   
   return (
-    <View style={{ flex: 1 }}>
+    <View style={Styles.screen}>
       <Text>code you layouts
         </Text> 
       
     </View>
   );
 };
+const styles = StyleSheet.create({
+  screen: {
+      flex: 1,
+      height: '100%',
+      justifyContent: 'center',
+      flexDirection: 'column',
+      alignItems: 'center',
+      margin: '1.5%',
+  },
+})
 
 export default Onboard_screen;
