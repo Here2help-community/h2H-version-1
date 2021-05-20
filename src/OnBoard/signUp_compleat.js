095029f11e75e7b5ca8141f7de4bc1cc34da7b2f
silenteyesoncode <46851135+silenteyesoncode@users.noreply.github.com>
2021-05-20 23:28:06 +0530

    cc

---
+++ src/OnBoard/signUp_compleat.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Image } from "react-native";
+import AppText from '../component/AppText/AppText';
+import styles from './OnboardStyles';
+
+const SignUp_Compleate = () => {
+  
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={styles.header}>
+        <View style={styles.header_image}>
+          <Image
+            source={require("./img/onb.png")}
+            resizeMode="contain"
+            style={{
+              width: "100%",
+              height: "100%",
+              alignSelf: "center",
+            }}
+          />
+        </View>
+      
+        <View style={styles.title}>
+          <AppText style={styles.head}>Make new friends locally</AppText>
+        </View>
+      </View>
+
+      <View style={styles.content}>
+        <AppText style={styles.text}>
+          Perfect way to meet new people in your neighborhood.
+        </AppText>
+      </View>
+    </View>
+  );
+};
+
+export default SignUp_Compleate;
